37a86246f61d3930113752126a5a3764cdc705b2
Sanchit Gnawali <sanchitgnawali@gmail.com>
2024-06-07 17:23:00 -0500

    initialized project

---
+++ src/test/java/io/sanchit/socialbook/SocialBookApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sanchit.socialbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
